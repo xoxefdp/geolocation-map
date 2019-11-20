dac79e18df30ebbdeb699741e59841f0f652c39e
José Diaz <xoxefdp@gmail.com>
2019-11-20 02:18:35 +0100

    Added vuejs templating, updated webpack rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
-const Webpack = require('webpack')
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin') // require('vue-loader')
 
-const DIST_DIR = path.resolve('dist')
-const SRC_DIR = path.resolve('src')
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const SRC_DIR = path.resolve(__dirname, 'src')
 // const NODE_DIR = path.resolve(__dirname, 'node_modules')
 
 const config = {
+  devtool: 'source-map',
   entry: {
     app: SRC_DIR + '/main.js'
   },
@@ -14,13 +16,61 @@ const config = {
     path: DIST_DIR,
     filename: 'bundle.js'
   },
+  resolve: {
+    extensions: [ '.js', '.vue' ],
+    alias: {
+      // 'vue$': isDev ? 'vue/dist/vue.runtime.js' : 'vue/dist/vue.runtime.min.js',
+      'vue$': 'vue/dist/vue.esm.js', // 'vue/dist/vue.common.js' for webpack 1
+      main: SRC_DIR
+    }
+  },
   plugins: [
-    new Webpack.HotModuleReplacementPlugin(),
+    new webpack.EnvironmentPlugin({ DEBUG: true }),
+    new webpack.HotModuleReplacementPlugin(),
+    new VueLoaderPlugin(),
     new HtmlWebPackPlugin({
       template: SRC_DIR + '/index.html',
       filename: 'index.html'
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/i,
+        use: [
+          {
+            loader: 'vue-loader',
+            // exclude: [ NODE_DIR ],
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.js$/i,
+        use: [
+          {
+            loader: 'babel-loader',
+            // exclude: [ NODE_DIR ],
+            options: { sourceMap: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            // exclude: [ NODE_DIR ],
+            options: { sourceMap: true }
+          }
+        ]
+      }
+    ]
+  },
   devServer: {
     hot: true,
     port: 9000
