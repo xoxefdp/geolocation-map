ff8af80f6fa2221a001b45ad3d89788070c0856e
José Diaz <xoxefdp@gmail.com>
2019-11-27 09:34:54 +0100

    Added App vue template to main

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import '@babel/polyfill'
 import * as L from 'leaflet'
 import Vue from 'vue'
-import App from 'main/components/App'
+import App from 'components/app/App'
 
 Vue.use(L)
 
 new Vue({
   el: '#root',
-  components: { App }
+  components: { App },
+  template: '<App />'
 })
