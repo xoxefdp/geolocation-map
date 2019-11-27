aaab7cc2e4664dc998eb13053cc5a2d0cc5dc657
José Diaz <xoxefdp@gmail.com>
2019-11-27 09:35:15 +0100

    Added App vue component

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -15,15 +15,13 @@
       <p>last coords update: <span id="lastCoordsUpdate"></span></p>
       <div style="display:inline-block">
         <div style="display:inline-block">
-          <button id="trackingButton" v-on:click="toggleTracking">
-            <span id="trackingOn" style="display:block">Track: On</span>
-            <span id="trackingOff" style="display:none">Track: Off</span>
+          <button v-on:click="toggleTracking">
+            <span>{{ trackingText }}</span>
           </button>
         </div>
         <div style="display:inline-block">
-          <button id="mapButton" style="display:none" v-on:click="toggleMap">
-            <span id="showMap" style="display:block">Map: On</span>
-            <span id="hideMap" style="display:none">Map: Off</span>
+          <button v-bind:class="{ btnShow: tracking, btnHide: !tracking }" v-on:click="toggleMap">
+            <span>{{ mapButtonText }}</span>
           </button>
         </div>
       </div>
@@ -32,6 +30,12 @@
 </template>
 
 <script>
+  import { getInstance as getLogger } from 'helpers/logger'
+  import geolocation from 'geolocation/geolocation'
+  import GeolocationEvent from 'geolocation/GeolocationEvents'
+  import { getFromStore } from 'systems/store'
+  import broadcast from 'broadcast/broadcast'
+
   // import Map from 'components/app/Map'
   // import Controls from 'components/app/Controls'
 
@@ -45,17 +49,10 @@
         countCoordsUpdate: 0,
         map: null,
         mapElement: null,
-        mapRendered: false,
         popup: null,
         mapZoom: 17,
-        navListener: null,
+
         mapMarker: null,
-        mapButton: null,
-        showMap: null,
-        hideMap: null,
-        trackingOn: null,
-        trackingOff: null,
-        tracking: false,
         latitude: null,
         longitude: null,
         altitude: null,
@@ -66,7 +63,12 @@
         timestamp: null,
         statusUpdate: null,
         coordsUpdatedTimes: null,
-        lastCoordsUpdate: null
+        lastCoordsUpdate: null,
+
+        tracking: false,
+        trackingText: 'Track: On',
+        mapRendered: false,
+        mapButtonText: 'Map: On'
       }
     },
     methods: {
@@ -82,7 +84,9 @@
       },
 
       _getCurrentPosition() {
-        return this.currentPosition
+        const currentPosition = getFromStore('storeGeo.currentPosition')
+        DEBUG && getLogger('App').debug('_getCurrentPosition()', currentPosition)
+        return currentPosition
       },
 
       _setCurrentPosition(position) {
@@ -104,8 +108,8 @@
 
       _setPosition(position) {
         this.countCoordsUpdate++
-        this._setCurrentPosition(position)
-        this._printCoordsToBody( this._getCurrentPosition() )
+        // this._setCurrentPosition(position)
+        this._printCoordsToBody( position )
       },
 
       _revealPosition(position) {
@@ -120,7 +124,7 @@
         this._revealPosition(position)
         this._setPosition(position)
         // !mapRendered && renderMap(position.coords)
-        this.mapRendered && this.updateMap(position.coords)
+        // this.mapRendered && this.updateMap(position.coords)
       },
 
       renderMap(coordinates) {
@@ -143,7 +147,7 @@
 
         this.updateMap(coordinates)
 
-        this.toggleMapButtonMessages()
+        this.toggleMapButtonText()
       },
 
       removeMap() {
@@ -152,22 +156,24 @@
         this.map.setAttribute('style', '')
         this.map.className = ''
         this.map.innerHTML = ''
-        this.toggleMapButtonMessages()
+        this.toggleMapButtonText()
       },
 
       toggleMap() {
-        this.mapRendered ? this.removeMap() : this.renderMap( this._getCurrentPosition().coords )
+        if (this.mapRendered) {
+          this.removeMap()
+        } else {
+          const position = this._getCurrentPosition()
+          DEBUG && getLogger('App').debug('toggleMap()', position)
+          const coords = position.coords
+          DEBUG && getLogger('App').debug('toggleMap()', coords)
+          this.renderMap( coords )
+        }
+      },
+
+      toggleMapButtonText() {
         this.mapRendered = !this.mapRendered
-      },
-
-      toggleMapButton() {
-        this.mapButton.style.display = this.mapButton.style.display == 'block' ? 'none' : ' block'
-      },
-
-
-      toggleMapButtonMessages() {
-        this.showMap.style.display = this.showMap.style.display == 'block' ? 'none' : ' block'
-        this.hideMap.style.display = this.hideMap.style.display == 'block' ? 'none' : ' block'
+        this.mapButtonText = this.mapRendered ? 'Map: Off' : 'Map: On'
       },
 
       onMapClick(e) {
@@ -184,29 +190,23 @@
         this.mapMarker.setLatLng(position)
       },
 
+
       enableTracking() {
-        this.toggleMapButton()
-        // this.navListener = navigator.geolocation.watchPosition(this.updateCurrentBody, this._positionDenied, null)
         geolocation.startTracking()
-        this.toggleTrackingButton()
       },
 
       disableTracking() {
-        this.toggleMap()
-        this.toggleMapButton()
-        // this.navListener = null
+        // this.toggleMap()
         geolocation.stopTracking()
-        this.toggleTrackingButton()
       },
 
       toggleTracking() {
         this.tracking ? this.disableTracking() : this.enableTracking()
-        this.tracking = !this.tracking
       },
 
-      toggleTrackingButton() {
-        this.trackingOn.style.display = this.trackingOn.style.display == 'block' ? 'none' : ' block'
-        this.trackingOff.style.display = this.trackingOff.style.display == 'block' ? 'none' : ' block'
+      toggleTrackingText() {
+        this.tracking = !this.tracking
+        this.trackingText = this.tracking ? 'Track: Off' : 'Track: On'
       }
     },
     beforeCreate: function() {
@@ -221,6 +221,11 @@
     mounted: function() {
       console.log('mounted')
 
+      broadcast.subscribe(GeolocationEvent.ON_GEOLOCATION_CURRENT_POSITION_UPDATE, this.updateCurrentBody)
+      broadcast.subscribe(GeolocationEvent.ON_GEOLOCATION_TRACKING_STARTED, this.toggleTrackingText)
+      broadcast.subscribe(GeolocationEvent.ON_GEOLOCATION_TRACKING_STOPPED, this.toggleTrackingText)
+
+
       this.latitude = document.getElementById('latitude')
       this.longitude = document.getElementById('longitude')
       this.altitude = document.getElementById('altitude')
@@ -232,22 +237,18 @@
       this.statusUpdate = document.getElementById('statusUpdate')
       this.coordsUpdatedTimes = document.getElementById('coordsUpdatedTimes')
       this.lastCoordsUpdate = document.getElementById('lastCoordsUpdate')
-      this.mapButton = document.getElementById('mapButton')
-      this.showMap = document.getElementById('showMap')
-      this.hideMap = document.getElementById('hideMap')
-      this.trackingOn = document.getElementById('trackingOn')
-      this.trackingOff = document.getElementById('trackingOff')
 
-      setInterval(() => {
-        let date = Date.now()
-        console.log(date)
-        this.statusUpdate.textContent = date
-        // console.log(geoPermissionStatus.state)
-        if (navigator.geolocation) {
-          // _printCurrentPosition()
-          this.lastCoordsUpdate.textContent = this.statusUpdate.textContent - this.timestamp.textContent
-        }
-      }, 1000)
+      // setInterval(() => {
+      //   let date = Date.now()
+      //   console.log(date)
+      //   this.statusUpdate.textContent = date
+      //   // console.log(geoPermissionStatus.state)
+      //   if (geolocation._isSupported()) {
+      //     // _printCurrentPosition()
+      //     this.lastCoordsUpdate.textContent = this.statusUpdate.textContent - this.timestamp.textContent
+      //     // this._getCurrentPosition();
+      //   }
+      // }, 1000)
     },
     beforeUpdate: function() {
       console.log('beforeUpdate')
@@ -260,11 +261,37 @@
     },
     destroyed: function() {
       console.log('destroyed')
+      broadcast.unsubscribe(GeolocationEvent.ON_GEOLOCATION_CURRENT_POSITION_UPDATE)
     }
   }
 
   export default App
 </script>
 <style>
+  @import "~leaflet/dist/leaflet.css";
 
+  #map {
+    padding: 10px;
+  }
+
+  #data-scheme {
+    background-color:rgba(255,255,255,0.6);
+    position: absolute;
+    bottom: 30px;
+    right: 10px;
+    z-index: 1000;
+    padding: 5px;
+    line-height: 0;
+  }
+  #latitude, #longitude, #altitude, #accuracy, #altitudeAccuracy, #heading, #speed, #coordsUpdatedTimes, #timestamp, #statusUpdate, #lastCoordsUpdate {
+    color: red;
+  }
+
+  .btnShow {
+    display: block;
+  }
+
+  .btnHide {
+    display: none;
+  }
 </style>
