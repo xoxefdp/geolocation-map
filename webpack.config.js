34afc06d0d4cc8b3fa067d437e77ddc39170465e
José Diaz <xoxefdp@gmail.com>
2019-11-27 09:32:19 +0100

    Added lodash library, added vuejs to project, updated webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,39 @@
 const path = require('path')
+// WEBPACK REQUIRES
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
-const VueLoaderPlugin = require('vue-loader/lib/plugin') // require('vue-loader')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+// const dvt = require(WEBPACK_COMMON_DIR + '/devtool-level')
 
-const DIST_DIR = path.resolve(__dirname, 'dist')
+// FRAMEWORK FOLDERS
+const NODE_DIR = path.resolve(__dirname, 'node_modules')
+const VUE_DIST_DIR = path.resolve(__dirname, NODE_DIR + '/vue/dist')
+// const WEBPACK_COMMON_DIR = path.resolve(__dirname, 'webpack')
+
+// APPLICATION FOLDERS
 const SRC_DIR = path.resolve(__dirname, 'src')
-// const NODE_DIR = path.resolve(__dirname, 'node_modules')
+const DIST_DIR = path.resolve(__dirname, 'dist')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const DevtoolLevel = {
+  NONE: 'none',
+  EVAL: 'eval',
+  CHEAP_EVAL_SOURCE_MAP: 'cheap-eval-source-map',
+  CHEAP_MODULE_EVAL_SOURCE_MAP: 'cheap-module-eval-source-map',
+  EVAL_SOURCE_MAP: 'eval-source-map',
+  CHEAP_SOURCE_MAP: 'cheap-source-map',
+  CHEAP_MODULE_SOURCE_MAP: 'cheap-module-source-map',
+  INLINE_CHEAP_SOURCE_MAP: 'inline-cheap-source-map',
+  INLINE_CHEAP_MODULE_SOURCE_MAP: 'inline-cheap-module-source-map',
+  SOURCE_MAP: 'source-map',
+  INLINE_SOURCE_MAP: 'inline-source-map',
+  HIDDEN_SOURCE_MAP: 'hidden-source-map',
+  NOSOURCES_SOURCE_MAP: 'nosources-source-map'
+}
 
 const config = {
-  devtool: 'source-map',
+  devtool: isDev ? DevtoolLevel.SOURCE_MAP : DevtoolLevel.CHEAP_SOURCE_MAP,
   entry: {
     app: SRC_DIR + '/main.js'
   },
@@ -19,13 +44,24 @@ const config = {
   resolve: {
     extensions: [ '.js', '.vue' ],
     alias: {
-      // 'vue$': isDev ? 'vue/dist/vue.runtime.js' : 'vue/dist/vue.runtime.min.js',
-      'vue$': 'vue/dist/vue.esm.js', // 'vue/dist/vue.common.js' for webpack 1
-      main: SRC_DIR
+      // 'vue$': isDev ? VUE_DIST_DIR + '/vue.common.js' : VUE_DIST_DIR + '/vue.runtime.common.js', // CommonJS
+      'vue$': isDev ? VUE_DIST_DIR + '/vue.esm.js' : VUE_DIST_DIR + '/vue.runtime.esm.js', // ESM (for bundlers)
+      // 'vue$' : isDev ? VUE_DIST_DIR + '/vue.esm.browser.js' : VUE_DIST_DIR + '/vue.esm.browser.min.js', // ESM (for browsers)
+      components: SRC_DIR + '/components',
+      helpers: SRC_DIR + '/helpers',
+      systems: SRC_DIR + '/systems',
+      broadcast: SRC_DIR + '/systems/broadcast',
+      geolocation: SRC_DIR + '/systems/geolocation',
+      styles: SRC_DIR + '/styles'
     }
   },
   plugins: [
-    new webpack.EnvironmentPlugin({ DEBUG: true }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: process.env.NODE_ENV // use 'development' unless process.env.NODE_ENV is defined
+    }),
+    new webpack.DefinePlugin({
+      DEBUG: isDev // use DEBUG unless process.env.DEBUG is defined
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new VueLoaderPlugin(),
     new HtmlWebPackPlugin({
@@ -37,37 +73,19 @@ const config = {
     rules: [
       {
         test: /\.vue$/i,
-        use: [
-          {
-            loader: 'vue-loader',
-            // exclude: [ NODE_DIR ],
-            options: { sourceMap: true }
-          }
-        ]
+        use: [ 'vue-loader' ]
       },
       {
         test: /\.js$/i,
-        use: [
-          {
-            loader: 'babel-loader',
-            // exclude: [ NODE_DIR ],
-            options: { sourceMap: true }
-          }
-        ]
+        use: [ 'babel-loader' ]
       },
       {
         test: /\.css$/i,
-        use: [ 'style-loader', 'css-loader' ]
+        use: [ 'vue-style-loader', 'style-loader', 'css-loader' ]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            // exclude: [ NODE_DIR ],
-            options: { sourceMap: true }
-          }
-        ]
+        use: [ 'file-loader' ]
       }
     ]
   },
@@ -76,5 +94,9 @@ const config = {
     port: 9000
   }
 };
+
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('DEBUG', isDev)
+console.log('devtool', config.devtool)
 
 module.exports = config
