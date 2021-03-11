430264d11671af697b334606e8e93862d5b76da4
José Diaz <xoxefdp@gmail.com>
2021-03-11 22:52:49 +0100

    Created module for the web Permission API, query geolocation permissions on init

--- src/initAsync.js
+++ src/initAsync.js
@@ -1,10 +1,20 @@
+import { isPermissionsSupported } from 'permissions/permissions'
+import { initGeoService } from 'geolocation/geolocation'
+
 const initAsync = () => {
   return new Promise(
     (resolve, reject) => {
       // INIT ANY MODULE BEFORE RENDER HERE
-      resolve()
-    }
-  )
+      isPermissionsSupported
+        ? initGeoService()
+          .then((permissionStatus) => {
+            resolve(permissionStatus)
+          })
+          .catch((error) => {
+            reject(error)
+          })
+        : reject(new Error('Web APIs not supported'))
+    })
 }
 
 export default initAsync
