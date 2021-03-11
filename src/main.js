614fce6d73c27fd5950a6a01c3b40e590b5d0d44
José Diaz <xoxefdp@gmail.com>
2021-03-11 23:02:38 +0100

    Removed the-browser-logger library

--- src/main.js
+++ src/main.js
@@ -1,18 +1,11 @@
-import { Level, setTimestampFormat, setLoggerLevel, requestLogger } from 'the-browser-logger'
-import initAsync from './initAsync'
+import initAsync from 'initAsync'
 import { createUI } from 'systems/bootUI'
 
-setTimestampFormat(true)
-DEBUG && setLoggerLevel(Level.DEBUG)
-
 const ID = 'main'
-const _getLogger = () => {
-  return requestLogger(ID)
-}
 
 initAsync().finally(
   () => {
-    _getLogger().debug('initAsync()')
+    console.debug(ID, 'initAsync()')
     createUI()
   }
 )
