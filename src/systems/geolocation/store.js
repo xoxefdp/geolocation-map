d1fb9ef9ad97da5a0c5a31ed042f3d314f14af09
José Diaz <xoxefdp@gmail.com>
2021-03-18 16:41:02 +0100

    Fixed log messages

--- src/systems/geolocation/store.js
+++ src/systems/geolocation/store.js
@@ -53,7 +53,7 @@ const setBeforeCurrentPosition = (position) => {
  */
 const getStoredTrackingWatcher = () => {
   const trackingWatcher = getFromStore(`${STORE_NAME}.trackingWatcher`)
-  console.debug(ID, `getStoredTrackingWatcher() ${trackingWatcher}`)
+  console.debug(ID, 'getStoredTrackingWatcher()', trackingWatcher)
   return trackingWatcher
 }
 
@@ -62,7 +62,7 @@ const getStoredTrackingWatcher = () => {
  */
 const getStoredInitialPosition = () => {
   const initialPosition = getFromStore(`${STORE_NAME}.initialPosition`)
-  console.debug(ID, `getStoredInitialPosition() ${initialPosition}`)
+  console.debug(ID, 'getStoredInitialPosition()', initialPosition)
   return initialPosition
 }
 
@@ -71,7 +71,7 @@ const getStoredInitialPosition = () => {
  */
 const getStoredCurrentPosition = () => {
   const currentPosition = getFromStore(`${STORE_NAME}.currentPosition`)
-  console.debug(ID, `getStoredCurrentPosition() ${currentPosition}`)
+  console.debug(ID, 'getStoredCurrentPosition()', currentPosition)
   return currentPosition
 }
 
@@ -80,7 +80,7 @@ const getStoredCurrentPosition = () => {
  */
 const getStoredBeforeCurrentPosition = () => {
   const beforeCurrentPosition = getFromStore(`${STORE_NAME}.beforeCurrentPosition`)
-  console.debug(ID, `getStoredBeforeCurrentPosition() ${beforeCurrentPosition}`)
+  console.debug(ID, 'getStoredBeforeCurrentPosition()', beforeCurrentPosition)
   return beforeCurrentPosition
 }
 
