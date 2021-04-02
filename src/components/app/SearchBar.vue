2b98d8d16a0708c1ccf2765be05433be7028865f
José Diaz <xoxefdp@gmail.com>
2021-04-02 20:32:45 +0200

    Redesigned search form, added methods lookUpAddress, lookUpLatLang and onFillAddress to deal with geo info api

--- src/components/app/SearchBar.vue
+++ src/components/app/SearchBar.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="search-bar">
-    <div class="inputs-container">
-      <div class="input-box">
-        <label for="latitude">Latitude</label>
-        <input type="number" name="latitude" id="latitude" v-model="latitude">
+    <div>
+      <div>
+        <input type="text" class="form-control" placeholder="ADDRESS" name="address" id="address" v-model="address">
       </div>
-      <div class="input-box">
-        <label for="longitude">Longitude</label>
-        <input type="number" name="longitude" id="longitude" v-model="longitude">
+      <div>
+        <button type="button" class="btn btn-primary" v-on:click="lookUpAddress">Search</button>
       </div>
-      <div class="input-box">
-        <button type="button" v-on:click="getLocation">Location</button>
+      <hr>
+      <div>
+        <input type="number" class="form-control" placeholder="LATITUDE" name="latitude" id="latitude" v-model="latitude">
       </div>
-      <div class="input-box">
-        <button type="button" :disabled="true">Search</button>
+      <div>
+        <input type="number" class="form-control" placeholder="LONGITUDE" name="longitude" id="longitude" v-model="longitude">
+      </div>
+      <div>
+        <button type="button" class="btn btn-primary" v-on:click="lookUpLatLang">Search</button>
       </div>
     </div>
   </div>
@@ -22,6 +24,7 @@
 <script>
 // EXTERNAL IMPORTS
 import { isNull } from 'the-type-validator'
+import PubSub from 'pubsub-js'
 // LOCAL IMPORTS
 import { getStoredCurrentPosition } from 'geolocation/store'
 
@@ -30,19 +33,77 @@ const SearchBar = {
   components: {},
   data: function() {
     return {
+      address: null,
       latitude: null,
       longitude: null,
     }
   },
   methods: {
-    getLocation () {
+    getCurrentPosition () {
       const currentPosition = getStoredCurrentPosition()
-      console.debug(SearchBar.name, 'getLocation()', currentPosition)
+      console.debug(SearchBar.name, 'getCurrentPosition()', currentPosition)
       if (!isNull(currentPosition)) {
         this.latitude = currentPosition.coords.latitude
         this.longitude = currentPosition.coords.longitude
       }
+      return currentPosition
     },
+    lookUpAddress () {
+      if (this.address) {
+        PubSub.publish('toggleLoading', true)
+        fetch(`/api/address/${this.address}`)
+          .then(response => response.json())
+          .then((data) => {
+            console.log(data)
+            this.latitude = data[0].latitude;
+            this.longitude = data[0].longitude;
+          }).catch((error) => {
+            console.log(error)
+          }).finally(() => {
+            PubSub.publish('locatePosition', { coords: {latitude: this.latitude, longitude: this.longitude}});
+            PubSub.publish('toggleLoading', false)
+          })
+      }
+    },
+    lookUpLatLang () {
+      if (this.latitude && this.longitude) {
+        PubSub.publish('toggleLoading', true)
+        fetch(`/api/latlang/${this.latitude}/${this.longitude}`)
+          .then(response => response.json())
+          .then((data) => {
+            console.log(data)
+            this.address = data[0].asciiname
+          }).catch((error) => {
+            console.log(error)
+          }).finally(() => {
+            PubSub.publish('locatePosition', { coords: {latitude: this.latitude, longitude: this.longitude}});
+            PubSub.publish('toggleLoading', false)
+          })
+      }
+    },
+    onFillAddress () {
+      const currentPosition = this.getCurrentPosition()
+      if (!isNull(currentPosition)) {
+        fetch(`/api/latlang/${this.latitude}/${this.longitude}`)
+          .then(response => response.json())
+          .then((data) => {
+            console.log(data)
+            this.address = data[0].asciiname
+          }).catch((error) => {
+            console.log(error)
+          })
+      }
+    },
+  },
+  mounted: function() {
+    console.debug(SearchBar.name, 'mounted')
+
+    PubSub.subscribe('fillAddress', this.onFillAddress)
+  },
+  beforeDestroy: function() {
+    console.debug(SearchBar.name, 'beforeDestroy')
+
+    PubSub.unsubscribe('fillAddress')
   },
 }
 export default SearchBar
@@ -50,48 +111,61 @@ export default SearchBar
 
 <style>
   .search-bar {
+    border-radius: 4px;
     top:0;
     left:0;
     position: absolute;
-    background-color: white;
+    background-color: rgba(0,0,0,0.1);
     color: black;
     margin: 8px;
     padding: 8px;
     z-index: 1000;
+    font-family: sans-serif;
   }
 
-  .inputs-container {
-    min-height: 60px;
-    display: -ms-flexbox;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-flex-direction: column;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    -webkit-flex-wrap: wrap;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-    -webkit-justify-content: space-around;
-    -ms-flex-pack: distribute;
-    justify-content: space-around;
-    -webkit-align-content: flex-start;
-    -ms-flex-line-pack: start;
-    align-content: flex-start;
-    -webkit-align-items: flex-start;
-    -ms-flex-align: start;
-    align-items: flex-start;
+  .form-control {
+    display: block;
+    padding: 6px 12px;
+    margin-bottom: 10px;
+    font-size: 15px;
+    line-height: 1.42857143;
+    color: #555555;
+    background-color: #ffffff;
+    background-image: none;
+    border: 1px solid #e7e7e7;
+    border-radius: 4px;
+    -webkit-box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
+    box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
+    -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+    -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
   }
 
-  .input-box {
-    width: 100%;
-    -webkit-order: 0;
-    -ms-flex-order: 0;
-    order: 0;
-    -webkit-flex: 0 1 auto;
-    -ms-flex: 0 1 auto;
-    flex: 0 1 auto;
-    -webkit-align-self: auto;
-    -ms-flex-item-align: auto;
-    align-self: auto;
+  .btn {
+    display: inline-block;
+    margin-bottom: 0;
+    font-weight: normal;
+    text-align: center;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    background-image: none;
+    border: 1px solid transparent;
+    white-space: nowrap;
+    padding: 6px 12px;
+    font-size: 15px;
+    line-height: 1.42857143;
+    border-radius: 4px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  .btn-primary {
+    background-color: #0078A8;
+    border-color: #0078A8;
+    color: #ffffff;
   }
 </style>
