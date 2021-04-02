5d776e72d1cc19d5c35cdca123018e3ec40809fe
José Diaz <xoxefdp@gmail.com>
2021-04-02 20:42:52 +0200

    Fixed linting problem

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(morgan('tiny'))
 
-// app.use('/', function (res, req) {
-//   // const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-//   console.log(req.connection)
-//   console.log(req.headers)
-// });
-
-app.use('/', express.static(__dirname + '/dist'));
+app.use('/', express.static(`${__dirname}/dist`))
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
