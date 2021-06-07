50c390fc5d05a03908f4b65601a3bf70df58cd43
Jose Diaz <xoxefdp@gmail.com>
2021-06-07 12:44:29 +0200

    Remove latitude and longitude fields, create a list of found references for address instead of reposition tofirst match

--- src/components/app/SearchBar.vue
+++ src/components/app/SearchBar.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="search-bar">
     <div>
-      <div>
-        <input type="text" class="form-control" placeholder="ADDRESS" name="address" id="address" v-model="address">
+      <div class="form-box">
+       <div class="form-address"><input type="text" class="form-control" placeholder="ADDRESS" name="address" id="address" v-model="address"></div>
+       <div class="form-search"><button type="button" class="btn btn-primary" v-on:click="lookUpAddress">Search</button></div>
       </div>
-      <div>
-        <button type="button" class="btn btn-primary" v-on:click="lookUpAddress">Search</button>
-      </div>
-      <hr>
-      <div>
-        <input type="number" class="form-control" placeholder="LATITUDE" name="latitude" id="latitude" v-model="latitude">
-      </div>
-      <div>
-        <input type="number" class="form-control" placeholder="LONGITUDE" name="longitude" id="longitude" v-model="longitude">
-      </div>
-      <div>
-        <button type="button" class="btn btn-primary" v-on:click="lookUpLatLang">Search</button>
+      <div v-show="items.length > 0">
+        <hr>
+        <div class="results-box">
+          <div class="result-item" v-for="item in items" :key="item.id">
+            <div class="result-address"><span>{{ item.asciiname }} / {{ item.country }} / {{ item.latitude }} / {{ item.longitude }}</span></div>
+            <div class="result-action"><button class="btn btn-primary right" v-on:click="goToAddress(item)">Go</button></div>
+          </div>
+        </div>
+        <hr>
+        <div class="results-info">
+          <span>RESULTS: {{ items.length }}</span>
+        </div>
       </div>
     </div>
   </div>
@@ -36,6 +37,7 @@ const SearchBar = {
       address: null,
       latitude: null,
       longitude: null,
+      items: [],
     }
   },
   methods: {
@@ -55,31 +57,19 @@ const SearchBar = {
           .then(response => response.json())
           .then((data) => {
             console.log(data)
-            this.latitude = data[0].latitude
-            this.longitude = data[0].longitude
+            this.items = data
           }).catch((error) => {
             console.log(error)
           }).finally(() => {
-            PubSub.publish('locatePosition', { coords: { latitude: this.latitude, longitude: this.longitude } })
             PubSub.publish('toggleLoading', false)
           })
       }
     },
-    lookUpLatLang () {
-      if (this.latitude && this.longitude) {
-        PubSub.publish('toggleLoading', true)
-        fetch(`/api/latlang/${this.latitude}/${this.longitude}`)
-          .then(response => response.json())
-          .then((data) => {
-            console.log(data)
-            this.address = data[0].asciiname
-          }).catch((error) => {
-            console.log(error)
-          }).finally(() => {
-            PubSub.publish('locatePosition', { coords: { latitude: this.latitude, longitude: this.longitude } })
-            PubSub.publish('toggleLoading', false)
-          })
-      }
+    goToAddress (item) {
+      this.latitude = item.latitude
+      this.longitude = item.longitude
+      this.items = []
+      PubSub.publish('locatePosition', { coords: { latitude: this.latitude, longitude: this.longitude } })
     },
     onFillAddress () {
       const currentPosition = this.getCurrentPosition()
@@ -89,6 +79,7 @@ const SearchBar = {
           .then((data) => {
             console.log(data)
             this.address = data[0].asciiname
+            this.items = data
           }).catch((error) => {
             console.log(error)
           })
@@ -111,11 +102,10 @@ export default SearchBar
 
 <style>
   .search-bar {
-    border-radius: 4px;
     top:0;
     left:0;
     position: absolute;
-    background-color: rgba(0,0,0,0.1);
+    background-color: slategray;
     color: black;
     margin: 8px;
     padding: 8px;
@@ -124,16 +114,10 @@ export default SearchBar
   }
 
   .form-control {
-    display: block;
-    padding: 6px 12px;
-    margin-bottom: 10px;
-    font-size: 15px;
-    line-height: 1.42857143;
     color: #555555;
     background-color: #ffffff;
     background-image: none;
     border: 1px solid #e7e7e7;
-    border-radius: 4px;
     -webkit-box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
     box-shadow: inset 0 1px 1px rgb(0 0 0 / 8%);
     -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
@@ -151,12 +135,9 @@ export default SearchBar
     touch-action: manipulation;
     cursor: pointer;
     background-image: none;
-    border: 1px solid transparent;
+    border: 0;
     white-space: nowrap;
-    padding: 6px 12px;
-    font-size: 15px;
-    line-height: 1.42857143;
-    border-radius: 4px;
+    text-transform: uppercase;
     -webkit-user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
@@ -168,4 +149,89 @@ export default SearchBar
     border-color: #0078A8;
     color: #ffffff;
   }
+
+  .results-box {
+    overflow: auto;
+    max-height: 200px;
+    background-color: white;
+  }
+
+  .result-item {
+    margin: 8px;
+    background-color: lightgray;
+  }
+
+  .result-address {
+    padding-left: 8px;
+  }
+
+  .results-info {
+    color: white;
+  }
+
+  .search-bar,
+  .form-control,
+  .btn,
+  .results-box,
+  .result-item {
+    border-radius: 4px;
+  }
+
+  .form-box,
+  .result-item {
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-flex-direction: row;
+    -ms-flex-direction: row;
+    flex-direction: row;
+    -webkit-flex-wrap: nowrap;
+    -ms-flex-wrap: nowrap;
+    flex-wrap: nowrap;
+    -webkit-justify-content: space-between;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    -webkit-align-content: stretch;
+    -ms-flex-line-pack: stretch;
+    align-content: stretch;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+  }
+
+  .form-control,
+  .btn {
+    padding: 6px 12px;
+    line-height: 1.42857143;
+  }
+
+  .form-address,
+  .result-address {
+    margin-right: 8px;
+  }
+
+  .form-address,
+  .form-search,
+  .result-address,
+  .result-action {
+    -webkit-order: 0;
+    -ms-flex-order: 0;
+    order: 0;
+    -webkit-flex: 0 1 auto;
+    -ms-flex: 0 1 auto;
+    flex: 0 1 auto;
+    -webkit-align-self: auto;
+    -ms-flex-item-align: auto;
+    align-self: auto;
+  }
+
+  /* @media (max-width: 600px) {
+    .search-bar {
+      top: unset;
+      left: unset;
+      bottom: 0;
+      margin: 0;
+      padding: 0;
+    }
+  } */
 </style>
